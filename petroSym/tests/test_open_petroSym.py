aab23534452f63571a1f162b2377097bb71a99e1
Santiago <santi_0926@hotmail.com>
2016-04-14 04:45:58 -0300

    Updating things in .travis.yml 12 and adding test for opening

---
+++ petroSym/tests/test_open_petroSym.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 04:40:26 2016
+
+@author: santiago
+"""
+import unittest
+from petroSym.petroSym import *
+import time
+import subprocess
+import signal
+
+class TestOpenGui(unittest.TestCase):
+   """ Test case for open_gui """
+   def test_open(self):       
+       #Ejecuto el comando petroSym durante 4 segundos y luego lo corto
+       p = subprocess.Popen(['petroSym'])
+       time.sleep(4)       
+       #p.poll() devuelve None si tuvo exito
+       assert(p.poll()==None)
+       #si llego hasta aca tuvo exito, entonces lo corto
+       if (p.pid):
+           os.kill(p.pid, signal.SIGKILL)
+       
+
+if __name__ == '__main__':
+    unittest.main()
