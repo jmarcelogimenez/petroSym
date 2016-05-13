c85ed5f1f526e9c86a996e50a35ff821e00a4182
Santiago <santi_0926@hotmail.com>
2016-05-12 22:55:06 -0300

    Adding perpetualTimer.py

---
+++ petroSym/perpetualTimer.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 30 12:06:05 2016
+
+@author: santiago
+"""
+
+from threading import Timer,Thread,Event
+
+class perpetualTimer():
+
+   def __init__(self,t,hFunction):
+      self.t=t
+      self.hFunction = hFunction
+      self.thread = Timer(self.t,self.handle_function)
+
+   def handle_function(self):
+      self.hFunction()
+      #self.thread = Timer(self.t,self.handle_function)
+      #self.thread.start()
+
+   def start(self):
+      self.thread.start()
+
+   def cancel(self):
+      self.thread.cancel()
